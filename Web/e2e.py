3bc0ccb6fae67f3cf196f07fea8d8d033d92a45e
shimon3 <shmouel6@gmail.com>
2023-08-21 16:05:45 +0300

    This is the web service

---
+++ Web/e2e.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+driver = webdriver.Chrome(service=Service(r'C:\Users\user\PycharmProjects\pythonProject1\ProjectWOG\Web\chromedriver.exe'))
+
+def test_scores_service():
+    driver.get("http://127.0.0.1:5000/")
+    score_element = int(driver.find_element_by_id("score").text)
+    if 1000 >= score_element >= 1:
+        return True
+    else:
+        return False
+
+
+def main_function():
+    # The main function will return -1 as an OS exit
+    # code if the tests failed and 0 if they passed.
+    test = test_scores_service()
+    if test:
+        return 0
+    else:
+        return -1
+
+
+main_function()
