f8f462f2033aac3b7aa3b7da6c5ab7bddedabd77
shimon3 <shmouel6@gmail.com>
2023-08-21 16:07:33 +0300

    You call here the updated main

---
+++ main.py
@@ -0,0 +1,11 @@
+from UpdatedMainGame import _welcome,load_game
+# from UpdatedMainGame import load_game
+
+#להציג את הWelcome
+_welcome()
+
+# #לעלות את המשחק
+load_game()
+
+# #לבקש איזה משחק המשתמש רוצה לשחק
+# get_number()
